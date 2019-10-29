46644f1fe37a5a8a4973478675ba7f901420efef
Elia Sternbuch <sternbuch.elijahu@hotmail.ch>
2019-10-29 15:12:07 +0100

    cloned from Compuskills samples

---
+++ src/Compuskills.Projects.TotalTimesheetPro.Domain/Models/Client.cs
@@ -0,0 +1,9 @@
+﻿namespace Compuskills.Projects.TotalTimesheetPro.Domain.Models
+{
+    public class Client
+    {
+        public int ClientID { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ src/Compuskills.Projects.TotalTimesheetPro.Domain/Models/TtpUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Compuskills.Projects.TotalTimesheetPro.Domain.Models
+{
+    public class TtpUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
